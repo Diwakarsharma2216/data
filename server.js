c63812af1cb811fbe2a79e0add75c07ef587c51b
Diwakar Sharma <diwakarsharma2216@gmail.com>
2023-04-04 09:00:55 +0530

    update-4

--- server.js
+++ server.js
@@ -1,10 +1,49 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router('db.json')
+// const middlewares = jsonServer.defaults()
  
-server.use(middlewares)
-server.use('', router)
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+// server.use(middlewares)
+// server.use('', router)
+// server.listen(process.env.PORT || 5000, () => {
+//   console.log('JSON Server is running')
+// })
+const express = require("express");
+
+const cors = require("cors");
+
+const bodyParser = require("body-parser");
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const config = new Configuration({
+  apiKey: "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",
+});
+
+const openai = new OpenAIApi(config);
+
+// Setup server
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// endpoint for ChatGPT
+
+app.post("/chat", async (req, res) => {
+  const { prompt } = req.body;
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    max_tokens: 512,
+    temperature: 0,
+    prompt: prompt,
+  });
+  res.send(completion.data.choices[0].text);
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
